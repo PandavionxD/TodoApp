75195b2e65d6f38f8d8667a4f6fea5edee7e2eb0
Alex <1151372@senati.pe>
2023-07-01 22:18:14 -0500

    terminando persistencia en localStorage

--- src/UseContext/ProviderContext.jsx
+++ src/UseContext/ProviderContext.jsx
@@ -2,40 +2,49 @@ import { useState } from "react";
 import { TodoContext } from "./TodoContext";
 
 export const ProviderContext = ({ children }) => {
-  const Todos_v1 = [
-    {
-      title: "Recolectar la piedra del alma",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-    {
-      title: "Recolectar la piedra del poder",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-    {
-      title: "Recolectar la piedra del tiempo",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-    {
-      title: "Recolectar la piedra de la mente",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-    {
-      title: "Recolectar la piedra del espacio",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-    {
-      title: "Recolectar la piedra de la realidad",
-      description: "Piedra ubicada en un lugar lejano en el espacio",
-      completed: false,
-    },
-  ];
+  // const Todos_v1 = [
+  //   {
+  //     title: "Recolectar la piedra del alma",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  //   {
+  //     title: "Recolectar la piedra del poder",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  //   {
+  //     title: "Recolectar la piedra del tiempo",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  //   {
+  //     title: "Recolectar la piedra de la mente",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  //   {
+  //     title: "Recolectar la piedra del espacio",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  //   {
+  //     title: "Recolectar la piedra de la realidad",
+  //     description: "Piedra ubicada en un lugar lejano en el espacio",
+  //     completed: false,
+  //   },
+  // ];
 
-  const [Todos, setTodos] = useState(Todos_v1);
+  const valueStorage = "TodoApp_v1";
+  const Local = localStorage.getItem(valueStorage);
+  let localStorageValue;
+  if (!Local) {
+    localStorage.setItem(valueStorage, JSON.stringify([]));
+    localStorageValue = [];
+  } else {
+    localStorageValue = JSON.parse(Local);
+  }
+  const [Todos, setTodos] = useState(localStorageValue);
   const [valueChange, setvalueChange] = useState("");
 
   let Todos1 = [];
@@ -50,30 +59,42 @@ export const ProviderContext = ({ children }) => {
     });
   }
 
-  const deleteTodo=(id)=>{
-    const Todos1 = [...Todos]
-    const index = Todos1.findIndex(todo=>{
-      return todo.title ===id
-    })
-    Todos1.splice(index,1)
-    setTodos(Todos1)
-  }
-
-  const completedTodo = (text)=>{
-    const Todos1 = [...Todos]
-    const index = Todos1.findIndex(todo=>{
-      return todo.title === text
-    })
-    Todos1[index].completed=!Todos1[index].completed
-    setTodos(Todos1)
-    
+  const PersitenciaLocalStorage = (newTodo)=>{
+    const stringyfi = JSON.stringify(newTodo)
+    localStorage.setItem(valueStorage,stringyfi)
+    setTodos(newTodo)
   }
 
 
+  const deleteTodo = (id) => {
+    const Todos1 = [...Todos];
+    const index = Todos1.findIndex((todo) => {
+      return todo.title === id;
+    });
+    Todos1.splice(index, 1);
+    return PersitenciaLocalStorage(Todos1)
+  };
+
+  const completedTodo = (text) => {
+    const Todos1 = [...Todos];
+    const index = Todos1.findIndex((todo) => {
+      return todo.title === text;
+    });
+    Todos1[index].completed = !Todos1[index].completed;
+    return PersitenciaLocalStorage(Todos1)
+  };
 
   return (
     <TodoContext.Provider
-      value={{ Todos, setTodos, valueChange, setvalueChange, Todos1,completedTodo ,deleteTodo}}
+      value={{
+        Todos,
+        setTodos,
+        valueChange,
+        setvalueChange,
+        Todos1,
+        completedTodo,
+        deleteTodo,
+      }}
     >
       {children}
     </TodoContext.Provider>
